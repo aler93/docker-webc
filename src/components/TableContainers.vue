546ae8ba9d899316a79adea603a559430f1f8758
Alisson Naimayer <anaimayer3@gmail.com>
2024-12-24 09:51:18 -0300

    Componente de tabela para visualizar containers

---
+++ src/components/TableContainers.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="table-containers">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TableContainers',
+  data() {
+    return{}
+  },
+  mounted() {
+    console.log("Mounted")
+  },
+}
+</script>
+
+<style scoped lang="scss">
+</style>
